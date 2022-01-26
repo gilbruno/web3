14ecac712770efe2fc96d22014a7d80e5142537c
Gilles Bruno <gilles.bruno@bappli.com>
2022-01-26 21:41:33 +0100

    Navbar building

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,28 @@
+import { HiMenuAlt4} from 'react-icons/hi';
+import { AiOutlineClose} from 'react-icons/ai';
+
+import logo from '../../images/cryptomango.png'
+
+const NavbarItem = ({title, classProps}) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}}`}>
+            {title}
+        </li>
+    )
+}
+
 const Navbar = () => {
     return (
-        <h1>NavBar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial juystify-center itels-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
+            </div>    
+            <ul className="text-white md:flex hiddent list-none flex-row justufiy-between items-center flex-initial">
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => {
+                    <NavbarItem key={item + index} title={item} />
+                })}
+            </ul>
+        </nav>
     )
 }
 
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,6 @@
 const Transactions = () => {
     return (
-        <h1>Services</h1>
+        <h1>Transactions</h1>
     )
 }
 
